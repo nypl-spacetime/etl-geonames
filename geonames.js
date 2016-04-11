6e22127491b4141ebe089cb28a648dcd91ea784a
Bert Spaan <bert.spaan@gmail.com>
2016-04-11 16:02:47 -0400

    new name, new etl module structure, standardjs

--- geonames.js
+++ geonames.js
@@ -1,14 +1,14 @@
-var fs = require('fs');
-var path = require('path');
-var request = require('request');
-var yauzl = require('yauzl');
-var H = require('highland');
-var R = require('ramda');
+var fs = require('fs')
+var path = require('path')
+var request = require('request')
+var yauzl = require('yauzl')
+var H = require('highland')
+var R = require('ramda')
 
 // GeoNames configuration
-var baseUrl = 'http://download.geonames.org/export/dump/';
-var baseUri = 'http://sws.geonames.org/';
-var allCountries = 'allCountries.zip';
+var baseUrl = 'http://download.geonames.org/export/dump/'
+var baseUri = 'http://sws.geonames.org/'
+var allCountries = 'allCountries.zip'
 
 var adminCodesFiles = [
   {
@@ -19,7 +19,7 @@ var adminCodesFiles = [
     key: 'admin2',
     filename: 'admin2Codes.txt'
   }
-];
+]
 
 var allCountriesColumns = [
   'geonameid',
@@ -41,14 +41,14 @@ var allCountriesColumns = [
   'dem',
   'timezone',
   'modificationDate'
-];
+]
 
 var adminCodeColumns = [
   'code',
   'name',
   'asciiname',
   'geonameid'
-];
+]
 
 var adminKeys = [
   'countryCode',
@@ -56,66 +56,56 @@ var adminKeys = [
   'admin2Code',
   'admin3Code',
   'admin4Code'
-];
+]
 
-function downloadGeoNamesFile(dir, filename, callback) {
+function downloadGeoNamesFile (dir, filename, callback) {
   request
     .get(baseUrl + filename)
     .pipe(fs.createWriteStream(path.join(dir, filename)))
-    .on('error', function(err) {
-      callback(err);
-    })
-    .on('finish', function() {
-      callback();
-    });
+    .on('error', callback)
+    .on('finish', callback)
 }
 
-function readAdminCodes(path, adminCodes, callback) {
+function readAdminCodes (path, adminCodes, callback) {
   H(fs.createReadStream(path))
     .split()
     .compact()
     .map(R.split('\t'))
     .map(R.zipObj(adminCodeColumns))
-    .each(function(obj) {
-      adminCodes[obj.code] = obj;
+    .each(function (obj) {
+      adminCodes[obj.code] = obj
     })
-    .errors(function(err) {
-      callback(err);
-    })
-    .done(function() {
-      callback();
-    });
+    .errors(callback)
+    .done(callback)
 }
 
-function getAdminCodes(config, dir, callback) {
+function getAdminCodes (config, dir, callback) {
   var adminCodes = {
     admin1: {},
     admin2: {}
-  };
+  }
 
   H(adminCodesFiles)
-    .map(function(adminCodeFile) {
-      return H.curry(readAdminCodes, path.join(dir, adminCodeFile.filename), adminCodes[adminCodeFile.key]);
+    .map(function (adminCodeFile) {
+      return H.curry(readAdminCodes, path.join(dir, adminCodeFile.filename), adminCodes[adminCodeFile.key])
     })
     .nfcall([])
     .parallel(2)
-    .errors(function(err) {
-      callback(err);
+    .errors(callback)
+    .done(() => {
+      callback(null, adminCodes)
     })
-    .done(function() {
-      callback(null, adminCodes);
-    });
 }
 
-function getRelations(config, adminCodes, obj) {
-  var relations = [];
+function getRelations (config, adminCodes, obj) {
+  var relations = []
 
-  var codes = R.filter(R.identity, R.values(R.pick(adminKeys, obj)));
+  var codes = R.filter(R.identity, R.values(R.pick(adminKeys, obj)))
   if (codes.length === 3) {
-    var parentObj = adminCodes.admin2[codes.join('.')];
+    var parentObj = adminCodes.admin2[codes.join('.')]
 
     if (obj.geonameid === parentObj.geonameid) {
-      parentObj = adminCodes.admin1[codes.slice(0, 2).join('.')];
+      parentObj = adminCodes.admin1[codes.slice(0, 2).join('.')]
     }
 
     relations = [
@@ -124,25 +114,25 @@ function getRelations(config, adminCodes, obj) {
         to: baseUri + parentObj.geonameid,
         type: config.relations.liesIn
       }
-    ];
+    ]
   }
 
   // TODO: add support for admin1 -> country relations!
 
-  return relations;
+  return relations
 }
 
-function process(config, writer, row, adminCodes, callback) {
-  var type;
-  var featureCode = row.featureCode;
+function process (config, writer, row, adminCodes, callback) {
+  var type
+  var featureCode = row.featureCode
 
   while (featureCode.length > 0 && !type) {
-    type = config.types[featureCode];
-    featureCode = featureCode.slice(0, -1);
+    type = config.types[featureCode]
+    featureCode = featureCode.slice(0, -1)
   }
 
   if (type) {
-    var data = [];
+    var data = []
 
     var pit = {
       uri: baseUri + row.geonameid,
@@ -165,113 +155,105 @@ function process(config, writer, row, adminCodes, callback) {
         admin3Code: row.admin3Code,
         admin4Code: row.admin4Code
       }
-    };
+    }
 
     data.push({
       type: 'pit',
       obj: pit
-    });
+    })
 
-    data = data.concat(getRelations(config, adminCodes, row).map(function(relation) {
+    data = data.concat(getRelations(config, adminCodes, row).map(function (relation) {
       return {
         type: 'relation',
         obj: relation
-      };
-    }));
+      }
+    }))
 
-    writer.writeObjects(data, function(err) {
-      callback(err);
-    });
+    writer.writeObjects(data, function (err) {
+      callback(err)
+    })
   } else {
-    callback();
+    callback()
   }
 }
 
-function filterRow(filter, row, extraUris) {
-  return R.whereEq(filter, row) || extraUris[row.geonameid];
+function filterRow (filter, row, extraUris) {
+  return R.whereEq(filter, row) || extraUris[row.geonameid]
 }
 
-function download(config, dir, writer, callback) {
-  var adminCodesFilenames = adminCodesFiles.map(function(c) {
-    return c.filename;
-  });
+function download (config, dirs, options, callback) {
+  var adminCodesFilenames = adminCodesFiles.map(function (c) {
+    return c.filename
+  })
 
   H([
     allCountries,
     adminCodesFilenames
   ])
     .flatten()
-    .map(H.curry(downloadGeoNamesFile, dir))
+    .map(H.curry(downloadGeoNamesFile, dirs.current))
     .nfcall([])
     .series()
-    .done(function() {
-      yauzl.open(path.join(dir, allCountries), {lazyEntries: true}, function(err, zipfile) {
+    .done(() => {
+      yauzl.open(path.join(dirs.current, allCountries), {lazyEntries: true}, function (err, zipfile) {
         if (err) {
-          throw err;
+          callback(err)
+          return
         }
 
-        zipfile.readEntry();
-        zipfile.on('entry', function(entry) {
-          var allCountriesTxt = allCountries.replace('zip', 'txt');
+        zipfile.readEntry()
+        zipfile.on('entry', function (entry) {
+          var allCountriesTxt = allCountries.replace('zip', 'txt')
           if (entry.fileName === allCountriesTxt) {
-            zipfile.openReadStream(entry, function(err, readStream) {
+            zipfile.openReadStream(entry, function (err, readStream) {
               if (err) {
-                throw err;
+                callback(err)
+              } else {
+                readStream
+                  .on('end', callback)
+                  .pipe(fs.createWriteStream(path.join(dirs.current, allCountriesTxt)))
               }
-
-              readStream
-                .pipe(fs.createWriteStream(path.join(dir, allCountriesTxt)))
-                .on('end', function() {
-                  callback();
-                });
-            });
+            })
           }
-        });
-      });
-    });
+        })
+      })
+    })
 }
 
-function convert(config, dir, writer, callback) {
-  getAdminCodes(config, dir, function(err, adminCodes) {
+function transform (config, dirs, tools, callback) {
+  getAdminCodes(config, dirs.previous, function (err, adminCodes) {
     if (err) {
-      callback(err);
-    } else {
-      var filename = path.join(dir, 'allCountries.txt');
-
-      var extraUris = {};
-      (config.extraUris ? require(config.extraUris) : []).forEach(function(uri) {
-        var id = uri.replace('http://sws.geonames.org/', '');
-        extraUris[id] = true;
-      });
-
-      H(fs.createReadStream(filename, {encoding: 'utf8'}))
-        .split()
-        .map(R.split('\t'))
-        .map(R.zipObj(allCountriesColumns))
-        .filter(function(row) {
-          return R.flip(R.any)(config.filters)(R.curry(filterRow)(R.__, row, extraUris));
-        })
-        .map(function(row) {
-          return H.curry(process, config, writer, row, adminCodes);
-        })
-        .nfcall([])
-        .series()
-        .errors(function(err) {
-          callback(err);
-        })
-        .done(function() {
-          callback();
-        });
+      callback(err)
+      return
     }
-  });
+
+    var filename = path.join(dirs.previous, 'allCountries.txt')
+
+    var extraUris = {}(config.extraUris ? require(config.extraUris) : []).forEach(function (uri) {
+      var id = uri.replace('http://sws.geonames.org/', '')
+      extraUris[id] = true
+    })
+
+    H(fs.createReadStream(filename, {encoding: 'utf8'}))
+      .split()
+      .map(R.split('\t'))
+      .map(R.zipObj(allCountriesColumns))
+      .filter(function (row) {
+        return R.flip(R.any)(config.filters)(R.curry(filterRow)(R.__, row, extraUris))
+      })
+      .map(function (row) {
+        return H.curry(process, config, tools.writer, row, adminCodes)
+      })
+      .nfcall([])
+      .series()
+      .errors(callback)
+      .done(callback)
+  })
 }
 
 // ==================================== API ====================================
 
-module.exports.title = 'GeoNames';
-module.exports.url = 'http://www.geonames.org/';
-
 module.exports.steps = [
   download,
-  convert
-];
+  transform
+]
